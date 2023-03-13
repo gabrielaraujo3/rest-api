9473154423e006c5f4961caaacf689a17b3add6c
Gabriel <araujog388@gmail.com>
2023-03-13 20:21:19 -0300

    criando jwt do usuário

--- src/models/User.js
+++ src/models/User.js
@@ -49,6 +49,11 @@ export default class User extends Model {
         user.password_hash = await bcryptjs.hashSync(user.password, 8);
       }
     });
+
     return this;
+  }
+
+  passwordIsValid(password) {
+    return bcryptjs.compare(password, this.password_hash);
   }
 }
