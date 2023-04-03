abcb50cda53e039b360a3be76897c5c2994199a0
Gabriel <araujog388@gmail.com>
2023-04-03 20:55:30 -0300

    correção erros

---
+++ dist/database/seeds/20230326201402-criar-usuarios.js
@@ -0,0 +1,35 @@
+"use strict";const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          nome: 'Luiz',
+          email: 'luiz@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Luiz2',
+          email: 'luiz2@gmail.com',
+          password_hash: await bcryptjs.hash('654321', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Luiz3',
+          email: 'luiz3@gmail.com',
+          password_hash: await bcryptjs.hash('147852369', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  // async down() {},
+};
